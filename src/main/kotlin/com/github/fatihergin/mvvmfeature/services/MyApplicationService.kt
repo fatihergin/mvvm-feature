2618579aacbbe5cbfb70d943dd5b0e01bb6a48d7
GitHub Action <action@github.com>
2021-02-02 07:02:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/fatihergin/mvvmfeature/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/fatihergin/mvvmfeature/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.fatihergin.mvvmfeature.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fatihergin.mvvmfeature.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/fatihergin/mvvmfeature/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fatihergin/mvvmfeature/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fatihergin.mvvmfeature.services
 
+import com.github.fatihergin.mvvmfeature.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
